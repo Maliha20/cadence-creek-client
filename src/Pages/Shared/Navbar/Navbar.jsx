3262ceedb04297a2217a34e3bb0ff7e43976dac0
maliha20 <gulista.marium20@gmail.com>
2023-06-07 10:18:18 +0600

    navbar made

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import logo from '../../../assets/logo.png'
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    const navLists = <>
+           <li className="hover:text-blue-900 ">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:text-blue-900 ">
+            <Link>Item 3</Link>
+          </li>
+    </>
+  return (
+    <div className="navbar bg-blue-400/70">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52"
+          >
+          {navLists}
+          
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">
+            <img className="md:w-56 w-36 h-28 md:h-36 md:mt-4" src={logo} alt="" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-2xl text-white font-semibold md:mt-12 ">
+        {navLists}
+        </ul>
+      </div>
+      <div className="navbar-end md:mr-5 md:mt-12">
+        <a className="btn btn-ghost px-1 md:px-5 text-white bg-blue-900">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,12 +1,20 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home/Home";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element:<MainLayout></MainLayout>,
+      children: [
+        {
+            path: "/",
+            element:<Home></Home>
+        }
+      ]
     },
 ]);
 
